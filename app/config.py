79019c7426dc779a32152e3a857ba66cf86db7fe
Onur T. <onur@tatimur.com>
2026-03-28 13:40:54 +0300

    fixed actionbase url added to config.
    this using for ntfy.

--- app/config.py
+++ app/config.py
@@ -1,11 +1,14 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     vikunja_api_url: str = "http://localhost:3456/api/v1"
     vikunja_api_token: str = ""
     ntfy_url: str = "http://localhost:8080/vikunja"
+    action_base_url: str
     log_level: str = "INFO"
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
 
 settings = Settings()
